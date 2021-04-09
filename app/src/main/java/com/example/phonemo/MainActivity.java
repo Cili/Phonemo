1fcb6c72ff94632558dce4b43cf9474dd424e679
annawyndham <awyndham@wpi.edu>
2021-04-09 13:22:28 -0400

    button

--- app/src/main/java/com/example/phonemo/MainActivity.java
+++ app/src/main/java/com/example/phonemo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button startButton = findViewById(R.id.startButton);
-        startButton.setOnClickListener(v -> {
-            Intent constructorPage = new Intent(MainActivity.this, PhonemeConstructor.class);
-            startActivity(constructorPage);
-        });
+    }
+
+    public void onCreation(View v){
+        Intent intent = new Intent(this, PhonemeConstructor.class);
+        startActivity(intent);
+
     }
 }
