ebe73ceb155470ab0eadab7a3c5e851eacfb0fef
Jon Cili <56412439+Cili@users.noreply.github.com>
2021-04-11 19:04:08 -0400

    Added LinkedList to store phonemes
    separate constructWord method was added to display String representation of the LinkedList to noname

--- app/src/main/java/com/example/phonemo/PhonemeConstructor.java
+++ app/src/main/java/com/example/phonemo/PhonemeConstructor.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
 public class PhonemeConstructor extends AppCompatActivity {
 
     private String symbol = "";
-    private StringBuilder word = new StringBuilder();
+    //private StringBuilder word = new StringBuilder();
+    private LinkedList<String> wordList = new LinkedList<>();
+    
     private int index = 0;
 
     @Override
@@ -20,51 +22,48 @@ public class PhonemeConstructor extends AppCompatActivity {
         setContentView(R.layout.activity_phoneme_constructor);
     }
 
-    public void phonemeSingleOnClick (View v){
-       symbol = ((Button) v).getText().toString().substring(0,1);
+    public void phonemeSingleOnClick(View v) {
+       symbol = ((Button) v).getText().toString().substring(0, 1);
        //play sound of phoneme
     }
 
-    public void phonemeDoubleOnClick (View v){
-        symbol = ((Button) v).getText().toString().substring(0,2);
+    public void phonemeDoubleOnClick(View v) {
+        symbol = ((Button) v).getText().toString().substring(0, 2);
         //play sound of phoneme
     }
 
-    public void phonemeTripleOnClick (View v){
-        symbol = ((Button) v).getText().toString().substring(0,3);
+    public void phonemeTripleOnClick(View v) {
+        symbol = ((Button) v).getText().toString().substring(0, 3);
         //play sound of phoneme
     }
 
-    public void addButtonOnClick (View v){
-        TextView noname = (TextView)findViewById(R.id.PhonemeDisplay);
-        index+=symbol.length();
-        word.insert(index,symbol);
-
-        noname.setText(word);
+    public void addButtonOnClick(View v) {
+        wordList.add(index++, symbol); //Fun Fact: you can increment and use index at the same time
+        constructWord();
     }
 
-    public void leftArrowOnClick(View v){
-        if (index!=0)
-        index--;
+    public void leftArrowOnClick(View v) {
+        if(index != 0)
+            index--;
     }
 
-    public void rightArrowOnClick(View v){
-        if (index<symbol.length())
+    public void rightArrowOnClick(View v) {
+        if(index < symbol.length())
             index++;
     }
 
-    public void backspaceOnClick(View v){
-        word.deleteCharAt(index-1);
-        //need to fix index
+    public void backspaceOnClick(View v) {
+        if(wordList.size() > 0) {
+            wordList.remove(--index); //Will first decrement index THEN run the deleteCharAt function. At least, it should do that...
+            constructWord();
+        }
     }
-
-
-
-
-
-
-
-
-
-
+        
+    public void constructWord() {
+        String phonemicWord = "";
+        TextView noname = (TextView) findViewById(R.id.PhonemeDisplay);
+        for(String str : wordList) {
+            phonemicWord += str;
+        }
+        noname.setText(phonemicWord);
 }
