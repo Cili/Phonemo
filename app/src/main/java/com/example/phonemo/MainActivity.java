ed84f58713648d2282accac00d48ae176a5a9597
Jon Cili <56412439+Cili@users.noreply.github.com>
2021-05-11 09:15:11 -0400

    Update MainActivity.java
    Removed Unnecessary Curly Braces

--- app/src/main/java/com/example/phonemo/MainActivity.java
+++ app/src/main/java/com/example/phonemo/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.phonemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,47 +20,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-
         readPhonemeData();
-
         final Controller aController = (Controller) this.getApplicationContext();
-
-        for(GraphemeClass q: aController.getGraphemes()){
+        
+        for(GraphemeClass q: aController.getGraphemes())
             Log.v("Main Activity", "symbols: "+q.getSymbol() +" graphemes: "+ q.getGraphemes()[0]);
-
-        }
     }
 
-    private void readPhonemeData(){
+    private void readPhonemeData() {
         InputStream is = getResources().openRawResource(R.raw.phonemicdataaquisition);
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-        //get the global controller class object
-        final Controller aController = (Controller) this.getApplicationContext();
+        final Controller aController = (Controller) this.getApplicationContext(); //gets the global controller class object
 
         String line = " ";
 
         try {
-            while((line = reader.readLine())!= null){
+            while((line = reader.readLine()) != null) {
                 String[] fields = line.split(",");
-
                 GraphemeClass graph = new GraphemeClass(fields);
                 aController.addGrapheme(graph);
-
-               Log.wtf("GraphemeConstructor", "check"+aController.getGraphemesAtPlace(aController.getGraphemes().size()-1).getSymbol()+", "+aController.getGraphemesAtPlace(aController.getGraphemes().size()-1).getGraphemes());
-
+               Log.wtf("GraphemeConstructor", "check"+aController.getGraphemesAtPlace(aController.getGraphemes().size() - 1).getSymbol() + ", " + aController.getGraphemesAtPlace(aController.getGraphemes().size()-1).getGraphemes());
             }
         }
-        catch (IOException e){
-            Log.wtf("GraphemeConstructor", "Error Reading Data on Line"+line);
-
+        catch (IOException e) {
+            Log.wtf("GraphemeConstructor", "Error Reading Data on Line" + line);
         }
     }
 
@@ -70,10 +56,8 @@ public class MainActivity extends AppCompatActivity {
      * Starts the Word Creation Activity
      * @param v the start button
      */
-    public void onCreation(View v){
+    public void onCreation(View v) {
         Intent intent = new Intent(this, PhonemeConstructor.class);
-        //intent.putExtra("graphemeArray", graphemeArray);
         startActivity(intent);
-
     }
 }
